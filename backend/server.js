afb340cfe01f2b9e3f6b88b335c5f360273cffb5
mariosemmler@mail.de <mariosemmler@mail.de>
2021-10-29 19:34:43 +0200

    Neu Aufsetzung
    Aufteilung in Front und Backend
    React app "installiert" Proxy eingestellt, dass anfragen weitergeleitet werden.
    
    Backend User Login Aufgenommen

--- backend/server.js
+++ backend/server.js
@@ -1,74 +1,20 @@
+﻿require('rootpath')();
 const express = require('express');
-const port = process.env.PORT || 8080;
 const app = express();
-const bycrpyt = require('bcrypt');
-app.use(express.json() );
-const db_con = require('./db_connection');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorHandler = require('_middleware/error-handler');
 
-const users = [];
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
+// api routes
+app.use('/users', require('./users/users.controller'));
 
+// global error handler
+app.use(errorHandler);
 
-app.get('/users', (req, res) => {
-res.json(users)
-
-})
-
-// async wird hier verwendet, da bycrypt auch die async library verwendet!
-// durch bcrpyt werden die Passwörter gehashed um die Passwörter später nicht im Klartext
-// in der Datenbank zu haben
-/* Per Post /users wird ein user gespeichert, welcher  sein Passwort
-und Nutzernamen eigeben kann, hierbei wird das Passwort
-gehashed + "gesalted" und erst anschließend gespeichert
-*/
-app.post('/users', async (req, res) => {
-
- try{
-
-   // await verwenden, da asynch funktion 
- const salt = await bycrpyt.genSalt()
- const hashedPassword = await bycrpyt.hash(req.body.password, 10)
- const user = {name: req.body.name, password: hashedPassword}
- const name = req.body.name;
- db_con.connection.query("INSERT INTO `user` ( `username`, `password`) VALUES ("user.name", 'adasasdasdaddad')")
- users.push(user)
- res.status(201).send()
-
- }
-catch{
-
-    res.status(500).send()
-}
-
-});
-
-//selbiges wie oben, auch async, da bcrypt verwendung 
-app.post('/users/login', async (req, res) => {
-const user = users.find(user => user.name = req.body.name)
-
-if (user == null){
- return res.status(400).send('Benutzer nicht gefunden');
-}
-try{
-if (await bycrpyt.compare(req.body.password, user.password)){
-
-    res.send('Erfolg, Sie sind eingeloggt')
-}
-else{
-
-    res.send('Falsche Passwort eingabe')
-}
-
-}
-catch{
-   res.status(500).send() 
-}
-
-
-
-})
-
-
-
-
-app.listen(port);
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 8080;
+app.listen(port, () => console.log('Server listening on port ' + port));
