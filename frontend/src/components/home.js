762901de4b311c19a72b87ebc43aa43a3986171d
Haensele <92301437+Haensele@users.noreply.github.com>
2021-11-03 09:24:21 +0100

    2 buttons in home
    2 buttons in home, derzeit ohne große Funktion

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
-
+import './css/home.css' 
 
 export default function home() {
-return(<div> Hallo Welt </div>)}
+return(<div> 
+        <div class="flex">
+  <form action="YABE.js" class="flex-item">
+    <button class="button">YABE</button>
+  </form>
+
+  <form action="User.js" class="flex-item">
+    <button class="button">User Marktplatz</button>
+  </form>
+</div>
+
+ </div>)};
+
+
