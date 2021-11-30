a3f790623d74e9d2146f25f081f797b224015924
Haensele <92301437+Haensele@users.noreply.github.com>
2021-11-30 17:56:07 +0100

    Artikelübersicht mit props und Countdown

--- frontend/src/components/artikelübersicht.js
+++ frontend/src/components/artikelübersicht.js
@@ -5,59 +5,86 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Countdown from 'react-countdown';
+
+
+function Cardx(props){
+    return(
+        <div className="Card">
+            
+            
+            
+            <Col>
+            <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={props.img} />
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Subtitle>{props.description}</Card.Subtitle>
+                <Button variant="primary">Zum Produkt</Button>
+                <Button variant="secondary">Bieten</Button>
+                <Card.Footer className="zeit">
+                    
+                
+                {
+                    <Countdown date={Date.now() + props.bidtime} />
+                }
+                
+                    
+                </Card.Footer>
+            </Card.Body>
+            </Card>
+    
+            </Col>
+            
+            
+        </div>
+
+         )}
+
+
+         
+
+
+
 
 export default function(){
     return(
-    <div>
+        <div>
         <Navigation/>
-        
-        
         <Row xs={1} md={3} className="g-4">
-         {Array.from({ length: 15 }).map((_, idx) => (
-        <Col>
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="frontend\public\logo.jpg" />
-        <Card.Body>
-            <Card.Title>Produt Titel</Card.Title>
-            <Card.Subtitle> Beschreiung hier </Card.Subtitle>
-            <Button variant="primary">Zum Produkt</Button>
-            <Button variant="secondary">Bieten</Button>
-            <Card.Footer className="zeit">verbleibende Zeit 24h</Card.Footer>
-        </Card.Body>
-        </Card>
-
-        </Col>
-        ))}
-</Row>
-
-
-       
-
         
+        <Cardx
+            img="https://th.bing.com/th/id/R.174d1d09fe1b5f15f427ea8411fe2a21?rik=GJ9zKw7M6IGbuA&pid=ImgRaw&r=0"
+            title="Produkt1"
+            description="Produkt1 Beschreibung"
+            bidtime={600000}
+        />
 
+        <Cardx
+            img="https://th.bing.com/th/id/R.174d1d09fe1b5f15f427ea8411fe2a21?rik=GJ9zKw7M6IGbuA&pid=ImgRaw&r=0"
+            title="Produkt2"
+            description="Produkt2 Beschreibung"
+            bidtime={900000}
+        />
 
+        <Cardx 
+            img="https://th.bing.com/th/id/R.174d1d09fe1b5f15f427ea8411fe2a21?rik=GJ9zKw7M6IGbuA&pid=ImgRaw&r=0"
+            title="Produkt3"
+            description="Produkt3 Beschreibung"
+            bidtime={1200000}
+        />
 
+        <Cardx 
+            img="https://th.bing.com/th/id/R.174d1d09fe1b5f15f427ea8411fe2a21?rik=GJ9zKw7M6IGbuA&pid=ImgRaw&r=0"
+            title="Produkt4"
+            description="Produt 4 Beschreibung"
+            bidtime={10000}
+        />
 
+        </Row>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    </div>
+        </div>
+        
+    
 
     )}
