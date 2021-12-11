bf5798a5ae4d17aa26bc59b9c4eed5c76751c1ea
MarioS040 <73691715+MarioS040@users.noreply.github.com>
2021-12-12 00:08:19 +0100

    Artikel Übersicht angepasst
    working

--- frontend/src/components/artikelübersicht.js
+++ frontend/src/components/artikelübersicht.js
@@ -1,74 +1,93 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import './css/artikelübersicht.css';
 import Navigation from './navbar';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { NavItem } from 'react-bootstrap';
+import userauth from './auth.js';
 
 
+class artikelübersicht extends Component{
+    constructor(props){
+    super(props);
+  
+    this.state = {
+        Articles: []
+      };
 
-const Articles = [
-
-    {id: 1, articleName: "Eine Kiste Bananen", articleDescription: "Eine Kiste Bananen, Ablaufdatum übermorgen", Price: "22,99€", timeforauctionE: "So. 29.12.2021"  },
-    {id: 2, articleName: "Zwei Kisten Bananen", articleDescription: "Zwei Kisten Bananen, Ablaufdatum übermorgen", Price: "50€", timeforauctionE: "So. 29.12.2021"  },
-    {id: 3, articleName: "Drei Kisten Bananen", articleDescription: "Drei Kisten Bananen, Ablaufdatum übermorgen", Price: "75€", timeforauctionE: "So. 29.12.2021"  },
-    {id: 4, articleName: "Vier Kisten Bananen", articleDescription: "Vier Kisten Bananen, Ablaufdatum übermorgen", Price: "100€", timeforauctionE: "So. 29.12.2021"  },
-    {id: 5, articleName: "Fünf Kisten Bananen", articleDescription: "Fünf Kisten Bananen, Ablaufdatum heute", Price: "125", timeforauctionE: "So. 30.12.2021"  }
-];
-
-
+      }
     
-
-// Funktion um Artikel zu "Bauen", keys dienen der Position im Array, sodass bei der Übergabe klar ist, an welcher Stelle die einzelnen Werte sind
-
-const ArticleO = (props) => {
-    return(
-        <div className="Card">
-                
-                    
-                    <Card style={{ width: 'flex', margin: '20px'}}>
-                        
-                        <Card.Body>
-
-                            
-                                <Card.Img variant="top" src="https://th.bing.com/th/id/R.174d1d09fe1b5f15f427ea8411fe2a21?rik=GJ9zKw7M6IGbuA&pid=ImgRaw&r=0" />
-                                <Card.Title key={2}>{props.articleName}</Card.Title>
-                                <Card.Text key={3}>{props.articleDescription}</Card.Text>
-                                <Card.Subtitle key={4}>aktueller Preis: {props.Price}</Card.Subtitle>
-                                <Card.Subtitle key={5}>Auktion endet am: {props.timeforauctionE}</Card.Subtitle>
-                                <Button variant="primary">Zum Produkt</Button>
-                                <Button variant="secondary">Bieten</Button>
-                
-                            
-
-                        </Card.Body>
-                        
-                    </Card>  
-                     
-                
-
-        </div>
-)}
-
 
          
 /* {Articles.map(props=>(
         <tr key={props.id}></tr> */
 
 // Darstellung auf Website, hierbei wird das Array gemappt und die Werte über vorherdefinierte props weitergegeben
-export default function(){
-    return(
-        <div>
-        
-            <Navigation/>
+  async componentWillMount(){
+   
+        let userdaten = await userauth();
+        let token = await userdaten.complusertoken;
 
+            fetch('http://localhost:3000/article/auction',{
+            method: 'GET',
+            headers: {"content-type": "application/json",
+                     "Authorization": token},
+              
+            }
+            
+            )
+            .then((response) => response.json())
+            .then((response) => {this.setState({Articles: response})})
+            
+           
+            }
+
+render(){
+
+
+// Funktion um Artikel zu "Bauen", keys dienen der Position im Array, sodass bei der Übergabe klar ist, an welcher Stelle die einzelnen Werte sind
+  const  ArticleO = (props) => {
+
+let imgpath = "http://localhost:3000/uploads/" + props.path
+        return(
+            <div className="Card">
+                    
+                        
+                        <Card style={{ width: 'flex', margin: '20px'}}>
+                            
+                            <Card.Body>
+    
+                                
+                                    <Card.Img variant="top" src= {imgpath} />
+                                    <Card.Title key={2}>{props.articleName}</Card.Title>
+                                    <Card.Text key={3}>{props.articleDescription}</Card.Text>
+                                    <Card.Subtitle key={4}>aktueller Preis: {props.Price} €</Card.Subtitle>
+                                    <Card.Subtitle key={5}>Auktion endet am: {props.timeforauctionE}</Card.Subtitle>
+                                    <Button variant="primary">Zum Produkt</Button>
+                                    <Button variant="secondary">Bieten</Button>
+                    
+                                
+    
+                            </Card.Body>
+                            
+                        </Card>  
+                         
+                    
+    
+            </div>
+    )}
+
+    return(
+        
+        <div>
+    
+            <Navigation/>
+        
             <Row xs={1} md={2} lg={3} className="g-4">
             
-            {Articles.map((props)=>{
+            {this.state.Articles.map((props)=>{
 
-                return <ArticleO articleName={props.articleName} articleDescription={props.articleDescription} Price={props.Price} timeforauctionE={props.timeforauctionE} />;
+                return <ArticleO articleName={props.articleName} articleDescription={props.articleDescription} Price={props.Price} timeforauctionE={props.timeforauctionE} path={props.path} />;
                 
             })}
 
@@ -79,4 +98,6 @@ export default function(){
         
     
 
-    )}
+    )}}
+
+    export default artikelübersicht
