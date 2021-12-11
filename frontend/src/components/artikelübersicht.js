8d53e743882c0c23534a4a7283ee923f8aa9a29e
Haensele <92301437+Haensele@users.noreply.github.com>
2021-12-11 02:16:23 +0100

    Update artikelübersicht.js

--- frontend/src/components/artikelübersicht.js
+++ frontend/src/components/artikelübersicht.js
@@ -3,43 +3,64 @@ import './css/artikelübersicht.css';
 import Navigation from './navbar';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Countdown from 'react-countdown';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { NavItem } from 'react-bootstrap';
 
 
-function Cardx(props){
+
+const Articles = [
+
+    {id: 1, articleName: "Eine Kiste Bananen", articleDescription: "Eine Kiste Bananen, Ablaufdatum übermorgen", Price: "22,99€", timeforauctionE: "So. 29.12.2021"  },
+    {id: 2, articleName: "Zwei Kisten Bananen", articleDescription: "Zwei Kisten Bananen, Ablaufdatum übermorgen", Price: "50€", timeforauctionE: "So. 29.12.2021"  },
+    {id: 3, articleName: "Drei Kisten Bananen", articleDescription: "Drei Kisten Bananen, Ablaufdatum übermorgen", Price: "75€", timeforauctionE: "So. 29.12.2021"  },
+    {id: 4, articleName: "Vier Kisten Bananen", articleDescription: "Vier Kisten Bananen, Ablaufdatum übermorgen", Price: "100€", timeforauctionE: "So. 29.12.2021"  }
+
+];
+
+
+    
+
+
+
+const ArticleO = (props) => {
     return(
+
+        
         <div className="Card">
             
             
-            
-            <Col>
-            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={props.img} />
-            <Card.Body>
-                <Card.Title>{props.title}</Card.Title>
-                <Card.Subtitle>{props.description}</Card.Subtitle>
-                <Button variant="primary">Zum Produkt</Button>
-                <Button variant="secondary">Bieten</Button>
-                <Card.Footer className="zeit">
-                    
-                
-                {
-                    <Countdown date={Date.now() + props.bidtime} />
-                }
+            {Articles.map(props=>(
+                <Row xs={1} md={1} className="g-4">
                 
                     
-                </Card.Footer>
-            </Card.Body>
-            </Card>
-    
-            </Col>
-            
-            
-        </div>
+                    <Card style={{ width: 'flex'}}>   
+                        <Card.Body>
 
-         )}
+                            <tr key={props.id}>
+                                <Card.Img variant="top" src="https://th.bing.com/th/id/R.174d1d09fe1b5f15f427ea8411fe2a21?rik=GJ9zKw7M6IGbuA&pid=ImgRaw&r=0" />
+                                <Card.Title key={2}>{props.articleName}</Card.Title>
+                                <Card.Text key={3}>{props.articleDescription}</Card.Text>
+                                <Card.Subtitle key={4}>aktueller Preis: {props.Price}</Card.Subtitle>
+                                <Card.Subtitle key={5}>Auktion endet am: {props.timeforauctionE}</Card.Subtitle>
+                                <Button variant="primary">Zum Produkt</Button>
+                                <Button variant="secondary">Bieten</Button>
+                
+                            </tr>
+
+                        </Card.Body>
+                    </Card>
+                    
+                </Row>
+                
+            ))}
+        
+        
+        </div>
+    )
+
+
+}
 
 
          
@@ -50,40 +71,11 @@ function Cardx(props){
 export default function(){
     return(
         <div>
-        <Navigation/>
-        <Row xs={1} md={3} className="g-4">
         
-        <Cardx
-            img="https://th.bing.com/th/id/R.174d1d09fe1b5f15f427ea8411fe2a21?rik=GJ9zKw7M6IGbuA&pid=ImgRaw&r=0"
-            title="Produkt1"
-            description="Produkt1 Beschreibung"
-            bidtime={600000}
-        />
-
-        <Cardx
-            img="https://th.bing.com/th/id/R.174d1d09fe1b5f15f427ea8411fe2a21?rik=GJ9zKw7M6IGbuA&pid=ImgRaw&r=0"
-            title="Produkt2"
-            description="Produkt2 Beschreibung"
-            bidtime={900000}
-        />
-
-        <Cardx 
-            img="https://th.bing.com/th/id/R.174d1d09fe1b5f15f427ea8411fe2a21?rik=GJ9zKw7M6IGbuA&pid=ImgRaw&r=0"
-            title="Produkt3"
-            description="Produkt3 Beschreibung"
-            bidtime={1200000}
-        />
-
-        <Cardx 
-            img="https://th.bing.com/th/id/R.174d1d09fe1b5f15f427ea8411fe2a21?rik=GJ9zKw7M6IGbuA&pid=ImgRaw&r=0"
-            title="Produkt4"
-            description="Produt 4 Beschreibung"
-            bidtime={10000}
-        />
-
-
-        </Row>
-
+            <Navigation/>
+        
+            <ArticleO></ArticleO>
+       
         </div>
         
     
