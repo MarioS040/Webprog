711d4de1498cb41d875e58a32df80463ceb867b6
BigSiimp <92037307+BigSiimp@users.noreply.github.com>
2022-02-08 14:10:06 +0100

    Update 1.1 Artikel

--- frontend/src/components/artikel.js
+++ frontend/src/components/artikel.js
@@ -4,80 +4,88 @@ import Navigation from './navbar';
 import userauth from './auth.js';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
+import { InputGroup, FormControl, Button } from 'react-bootstrap'
 
 
 const Artikel = () => {
 
     const { id } = useParams();
     const [artikel, setArtikel] = useState({});
+    const [preis, setPreis] = useState('');
+
+    
 
     useEffect(() => {    // Update the document title using the browser API
-        
+
         const getArtikel = async () => {
-            console.log('Get Artikel works: ' + id);
             let userdaten = await userauth();
             let token = await userdaten.complusertoken;
-    
-            fetch(`http://localhost:3000/article/${id}`,{
-            method: 'GET',
-            headers: {"content-type": "application/json",
-                        "Authorization": token},
+            fetch(`http://localhost:3000/article/${id}`, {
+                method: 'GET',
+                headers: { "content-type": "application/json", "Authorization": token }
             })
-            .then((response) => response.json())
-            .then((artikel) => setArtikel(artikel))
+                .then((response) => response.json())
+                .then((artikel) => setArtikel(artikel))
+                console.log(artikel)
         };
         getArtikel();
     }, [id]);
 
-  return <div>
+    const onClick = async () => {
+        console.log('preis: ' + preis)
+        let userdaten = await userauth();
+        let token = await userdaten.complusertoken;
+        fetch(`http://localhost:3000/article/bieten/${id}`, {
+            method: 'POST',
+            headers: { "content-type": "application/json", "Authorization": token },
+            data: { "price": preis }
+        }).then((response) => console.log(response.json()))
+    }
 
-        <Navigation/>
+    return <div>
 
-        <div class = "produkt">
+        <Navigation />
 
-        <header>
-            <hgroup>
-                <h1>{artikel.articleName}</h1>
-                <h4> Das beste X überhaupt </h4>
-            </hgroup>
-        </header>
-        
-        <figure>
-            <img></img>
-        </figure>
+        <div class="produkt">
 
-        <section>
+            <header>
+                <hgroup>
+                    <h1>{artikel.articleName}</h1>
+                    <h4> Das beste X überhaupt </h4>
+                </hgroup>
+            </header>
 
-            <p> 
-            Lorem Ipsum bla bla. Lorem ipsum dolor sit amet, consetetur sadipscing elitr. Lorem ipsum dolor sit amet, consetetur sadipscing elitr
-            </p>
+            <figure>
+                <img src={'http://localhost:3000/uploads/' + artikel.path}></img>
+            </figure>
 
-            <details> 
-                <summary> Produkt Beschreibung </summary>
-                <ul>
-                    <li> Lorem ipsum dolor sit amet, consetetur sadipscing elitr </li>
-                    <li> Lorem ipsum dolor sit amet, consetetur sadipscing elitr </li>
-                    <li> Lorem ipsum dolor sit amet, consetetur sadipscing elitr </li>
-                </ul>
-            </details>
+            <section>
 
-            <button> Bieten </button>
-        </section>
+                <p>
+                    Lorem Ipsum bla bla. Lorem ipsum dolor sit amet, consetetur sadipscing elitr. Lorem ipsum dolor sit amet, consetetur sadipscing elitr
+                </p>
+
+                <details>
+                    <summary> Produkt Beschreibung </summary>
+                    <ul>
+                        <li>Preis: {artikel.Price}€</li>
+                        <li> Lorem ipsum dolor sit amet, consetetur sadipscing elitr </li>
+                        <li> Lorem ipsum dolor sit amet, consetetur sadipscing elitr </li>
+                    </ul>
+                </details>
+
+                <InputGroup className="mb-3">
+                    <FormControl placeholder="Dein Angebot" onChange={(e) => setPreis(e.target.value)}/>
+                    <Button onClick={onClick} variant="outline-secondary" id="button-bieten">
+                        Bieten
+                    </Button>
+                </InputGroup>
+
+            </section>
         </div>
 
 
-  </div>;
+    </div>;
 };
 
 export default Artikel;
-
-// export default function(){
-//     return(
-
-//     <div>
-        
-
-//     </div>
-
-//     )
-// }
